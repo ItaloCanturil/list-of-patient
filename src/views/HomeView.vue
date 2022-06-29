25a55ac84eb229477d4dd67e3ba48e5f38ec8f0e
Italo Canturil <italo.cantur@gmail.com>
2022-06-29 10:14:50 -0300

    feat: add table component and vuex logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="home">
+  <div class="home flex flex-col items-center">
   <h1>Home</h1>
+  <Table :data="users" class="my-3"/>
+  <button
+    class="rounded p-1 bg-slate-700 text-white"
+    @click="getMore(pages.results + 50)"
+  >
+    Loading more
+  </button>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex';
+import Table from '@/components/Table.vue';
 
 export default {
   name: 'HomeView',
-
+  components: {
+    Table,
+  },
   computed: {
     ...mapState([
       'users',
+      'pages',
     ]),
   },
   created() {
     this.getUsers();
   },
   methods: {
-    ...mapActions(['getUsers']),
+    ...mapActions(['getUsers', 'getMore']),
   },
 };
 </script>

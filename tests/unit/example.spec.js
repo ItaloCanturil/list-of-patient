b00ecbe9b944d0d79b3ba8a516fa2c38ae813cb2
Italo Canturil <italo.cantur@gmail.com>
2022-08-08 21:45:02 -0300

    feat: Add new test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Table from '@/components/Table.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('Table props', () => {
+  it('Renders if table is true', () => {
+    const wrapper = shallowMount(Table);
+    expect(wrapper.is(Table)).toBe(true);
   });
 });
